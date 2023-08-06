a61a6e3ea145a3b832300dfc6f54c8d7102f02cb
Jakub <devcobbier@gmail.com>
2023-08-06 17:10:09 +0200

    fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { OffcanvasComponent } from './components/offcanvas/offcanvas.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { LogoComponent } from './components/logo/logo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    OffcanvasComponent,
+    MenuComponent,
+    LogoComponent
   ],
   imports: [
     BrowserModule,
